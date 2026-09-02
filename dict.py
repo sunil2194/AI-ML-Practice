5c2ac6af523f1100ebcc63570e652b8f7f5a659f
Sunil Kalidindi <sunil4tibco@example.com>
2026-09-02 12:54:03 +0530

    Add python string and data type practice files

---
+++ dict.py
@@ -0,0 +1,6 @@
+# A dictionary storing user profile info
+user = {"name": "Alice","age": 30,"city": "Hyderabad"}
+
+# You look up information using the key instead of an index number
+print(user["name"])
+# Output: Alice
---
+++ list.py
@@ -0,0 +1,14 @@
+# A list is an orderd mutable collection of items. Lists are defined by having values between square brackets [ ].
+groceries = ["milk", "eggs", "bread", "eggs"]
+
+#print(groceries)
+#print(groceries[0])  # Output: milk
+#print(groceries[1])  # Output: eggs
+#print(groceries[2])  # Output: bread
+print(groceries[3])  # Output: eggs
+
+# You can change an item (lists are mutable)
+#groceries[0] = "almond milk"
+
+#print(groceries)
+# Output: ['almond milk', 'eggs', 'bread', 'eggs']
