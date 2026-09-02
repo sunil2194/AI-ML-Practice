0e6375280f09b6438cbfe224b1c100060cb85b11
Sunil Kalidindi <sunil4tibco@example.com>
2026-09-02 18:25:07 +0530

    Update format.py with tuple and print notes

--- list.py
+++ list.py
@@ -3,13 +3,13 @@
 # List can have duplicate values and can be of any data type (string, int, float, boolean, etc.)
 groceries = ["milk", "eggs", "bread", "eggs"]
         #       0       1       2       3
-#print(groceries)
+print(groceries)
 #print(groceries[0])  # Output: milk
 #print(groceries[1])  # Output: eggs
 #print(groceries[2])  # Output: bread
 #print(groceries[3])  # Output: eggs
 
-# You can change an item (lists are mutable-object can be added,modified/update)
+# You can change an item (lists are mutable-object can be added,modified/updated)
 #groceries[0] = "almond milk"
 
 #print(groceries)
